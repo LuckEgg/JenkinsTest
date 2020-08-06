613022bfc31fb3fff877702db4773739671073d0
sunbaocai <sun19881202@163.com>
2020-08-06 16:46:49 +0800

    修改

--- JenkinsTest/Pods/Target Support Files/Pods-JenkinsTest/Pods-JenkinsTest-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JenkinsTest : NSObject
-@end
-@implementation PodsDummy_Pods_JenkinsTest
-@end
--- JenkinsTest/Pods/Target Support Files/Pods-JenkinsTestTests/Pods-JenkinsTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JenkinsTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_JenkinsTestTests
-@end
